abb39d28a420a9f20bbfba38f2a4306383a59fa7
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2022-12-26 17:34:59 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rhyming!
+
+[https://master.d16qayanzycbk6.amplifyapp.com/](https://master.d16qayanzycbk6.amplifyapp.com/)
+
+
+- [x] Add word highlighting
+- [ ] Add syllable highlighting
+- [ ] Add a way to connect two groups with the same color
+- [ ] Add a way to see all the words that rhyme when you hover
---
+++ src/App.tsx
@@ -0,0 +1,127 @@
+import { Box, Button, Grid, Slider, TextField } from "@mui/material";
+import { useState } from "react";
+
+function App() {
+  const [text, setText] = useState("Alfred Lord Tennyson wrote this poem as a requiem for his college friend Arthur Henry Hallam who died suddenly at the age of 22. The poem was published almost 21 after Arthur’s death and tracks the journey of Tennyson’s grief. The length of the poem, 2892 lines, adds to the meaning by emphasizing the greatness of the poet’s sadness and how Tennyson continued to mourn his friend throughout his life. The poem includes iconic lines that have become common cliches, like: “‘Tis better to have loved and lost / Than never to have loved at all.” The poem is a good example of British Victorian poetry, both in terms of meter and form. But it also emphasizes friendship as a relationship as important and meaningful as romantic love.");
+
+  const [wordsPerMinute, setWordsPerMinute] = useState(60)
+
+  const [wordIndex, setWordIndex] = useState(0)
+
+  const [play, setPlay] = useState(false)
+
+  const [nextWordInterval, setNextWordInterval] = useState<any>()
+
+  const getWords = () => {
+    return text.split(" ").map((each) =>
+        each
+          .toLowerCase()
+          .replace(/[^\w\s\']|_/g, "")
+          .replace(/\s+/g, " ")
+      )
+  
+  };
+
+
+  const changePlay = () => {
+
+    if (play) {
+      // stop interval
+
+      clearInterval(nextWordInterval)
+      setNextWordInterval(undefined)
+    }
+
+    else {
+      // start interval
+
+      const waitTime = 60 / (wordsPerMinute) * 1000
+
+      const i = setInterval(() => {
+        setWordIndex(w => {
+          if (w === getWords().length) return 0
+          return w+1
+        })
+      }, waitTime)
+
+      setNextWordInterval(i)
+
+      
+    }
+
+    setPlay(!play)
+  }
+
+  const pause = () => {
+
+    setPlay(false)
+    clearInterval(nextWordInterval)
+    setNextWordInterval(undefined)
+  }
+
+  const handleWordsPerMinuteChange = (event: Event, newValue: number | number[]) => {
+    pause()
+    setWordsPerMinute(newValue as number);
+  };
+
+  const handleWordIndexChange = (event: Event, newValue: number | number[]) => {
+    pause()
+    setWordIndex(newValue as number);
+  };
+
+  return (
+    <Box sx={{ margin: "32px" }}>
+      <TextField
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        multiline
+        fullWidth
+        label="Add text!"
+      ></TextField>
+      <Button onClick={() => changePlay()} variant="contained" sx={{ mt: 2 }}>
+        {play ? "Pause" : "Play"}
+      </Button>
+
+      <br/>
+      
+      <p>Words Per Minute: {wordsPerMinute}</p>
+      <Slider value={wordsPerMinute} onChange={handleWordsPerMinuteChange} sx={{width: "300px"}} min={60} step={60}
+  marks
+  max={600}/>
+
+<p>Word Index: {wordIndex}</p>
+      <Slider value={wordIndex} onChange={handleWordIndexChange} sx={{width: "300px"}} min={0}
+  max={getWords().length}/>
+
+      <Box>
+        <h1>{getWords()[wordIndex]}</h1>
+      </Box>
+     
+      <Box m={4}>
+        <Grid container spacing={1}>
+
+        
+        {getWords().map((word, i) => {
+                
+          return (
+            <Grid xs={1} item sx={{ background: i === wordIndex ? "red" : "inherit" }} key={word}>
+              <Box
+                style={{
+                  margin: "4px",
+                  marginLeft: "8px",
+                  marginRight: "8px",
+                }}
+              >
+                {word}
+              </Box>
+            </Grid>
+          );
+        })}
+        </Grid>
+      </Box>
+
+    </Box>
+  );
+}
+
+export default App;
